86e74cce8b1b12fdffc61767bd27dcfad40b3db9
Dan Mallott <dmallott@westmonroepartners.com>
2016-08-03 18:59:12 -0500

    Attempting to get build to work in Azure Web App

---
+++ server.js
@@ -0,0 +1,12 @@
+﻿var express = require('express');
+var app = express();
+var port = process.env.port || 1337;
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+});
